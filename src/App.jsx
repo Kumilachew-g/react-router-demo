6f2f080ec1f19401f3674035a6af9fdd605dfdf2
Kumilachew-g <kumilachew1212@gmail.com>
2024-02-26 12:12:45 +0300

    Set up router in the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './Routes'; // Define your routes in a separate file
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return;
-  <></>;
+  return (
+    <Router>
+      <div>
+        <Routes />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
